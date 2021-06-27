18e879f514ecdab8a2c2a4a350f8e918446a7927
Monica saleth delapierre A <83409650+monicasaleth2000@users.noreply.github.com>
2021-06-27 20:12:44 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Info(){
+  return (
+    <div className="note">
+     <h1 class>Javascript and React.js</h1>
+     <p class>A basic webdevelopment React Js Bootcamp</p>
+    </div>
+  );
+}
+  
+export default Info;
